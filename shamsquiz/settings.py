09536555293829c8be5f1c272f43ad9206cd7620
Theghostscurity <pricejohnb6@gmail.com>
2026-09-22 15:35:36 +0300

    mbili

--- shamsquiz/settings.py
+++ shamsquiz/settings.py
@@ -57,7 +57,7 @@ AUTH_USER_MODEL = 'quiz.User'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
